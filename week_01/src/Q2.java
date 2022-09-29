5dd6a1441f639ce6b88f4310a5bdbc93e5552f80
kusal madura yapa <kusalmadurayapa@gmail.com>
2022-09-29 16:04:32 +0530

    file add the my tut 1 week 01

---
+++ week_01/src/Q2.java
@@ -0,0 +1,21 @@
+public class Q2 {
+        //2 Java/Netbeans Reminder
+        //    Write a program which displays the message
+        //    Welcome to Online Learning!
+        //    Please virus go away!
+        //    Make sure that the second message ”Please virus go away!” is displayed after a blank line is
+        //    between the first and the second sentence.
+
+    public static void main(String[] args) {
+
+//        System.out.println("Welcome to Online Learning!\n" +
+//                "\n\nPlease virus go away!");
+
+        System.out.println("Welcome to Online Learning!\n" +
+                "\n" +
+                "\nPlease virus go away!");
+
+    }
+
+}
+
---
+++ week_01/src/Q3.java
@@ -0,0 +1,16 @@
+public class Q3 {
+
+
+    // 3 Java/Netbeans Reminder - Syntax of the main method
+    //Your tutor will go through the syntax of the main method. What the arguments String[] args
+    //are and how you can pass command line arguments to the main method using Netbeans or the
+    //command line.
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+}
