b890ee4a6be199336cb8cb113799e1f505e70ca4
kusal madura yapa <kusalmadurayapa@gmail.com>
2022-11-09 09:10:51 +0530

    nnn

---
+++ src/week_5/CalcManager.java
@@ -0,0 +1,19 @@
+package week_5;
+
+import java.util.Scanner;
+
+public class CalcManager {
+
+    Scanner input = new Scanner(System.in);
+    int n = input.nextInt();
+// ctr alter l
+    public static int even(int n) {
+        Boolean b;
+        if (n % 2 == 0) {
+            b = true;
+        }
+        return n;
+    }
+
+
+}
