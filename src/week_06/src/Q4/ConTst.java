88a97866b14de9bab512cf452c3d0c6a4e9e6e45
kusal madura yapa <kusalmadurayapa@gmail.com>
2022-11-23 20:55:15 +0530

    my

--- src/week_06/src/Q4/ConTst.java
+++ src/week_06/src/Q4/ConTst.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
     class Book {
         public String author;
         public String title;
+        public int h;
     }
     public class ConTst {
         public static void main(String[] args) {
@@ -20,5 +21,6 @@ import java.util.ArrayList;
             Integer b3 = (Integer) list.get(1);
             Book b2 = (Book) list.get(2);
         }
+
     }
 
